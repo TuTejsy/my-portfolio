b0b3dfad45d2d3c72ae0a69ed8790c63c11641f8
TuTejsy <1111kaktus1111@gmail.com>
2019-07-05 23:07:55 +0300

    feat: add adaptive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,30 @@
 function addOnScreenEvent() {
     const pageHeaderContainer = document.querySelector('.page-header .header-container');
-    const hero = document.querySelector('.page-header .hero');
-    const heroStyle = getComputedStyle(hero);
-    const heroHeight = +heroStyle.height.slice(0, -2);
+    const headerOffSetTop = pageHeaderContainer.offsetTop;
+    const aboutMe = document.querySelector('.about-me');
+    const aboutMeOffSetTop = aboutMe.offsetTop;
     let isTransform = true;
 
     const onScreenEvent = () => {
-        if (isTransform && (window.pageYOffset + window.innerHeight + 10 >= 2 * heroHeight)) {
+        if (isTransform && (window.pageYOffset >= headerOffSetTop)) {
             isTransform = false;
             pageHeaderContainer.style.position = 'fixed';
             pageHeaderContainer.style.top = '0';
             pageHeaderContainer.classList.remove('white');
-        } else if (!isTransform && (window.pageYOffset + window.innerHeight + 10 < 2 * heroHeight)) {
+        } else if (!isTransform && (window.pageYOffset < headerOffSetTop)) {
             isTransform = true;
             pageHeaderContainer.style.position = 'relative';
             pageHeaderContainer.classList.add('white');
+        }
+
+        if (!isTransform && (window.pageYOffset + 50 >= aboutMeOffSetTop)) {
+            pageHeaderContainer.style.top = '';
+            pageHeaderContainer.style.bottom = '0';
+            pageHeaderContainer.classList.add('white');
+        } else if (!isTransform) {
+            pageHeaderContainer.style.top = '0';
+            pageHeaderContainer.style.bottom = '';
+            pageHeaderContainer.classList.remove('white');
         }
     }
 
@@ -34,11 +44,14 @@ function addParallax() {
     }
 
     const parallaxEvent = (e) => {
+
+        parallax1.style.animationDuration = '0s';
         parallax1.style.backgroundPositionX = `calc(50% + ${(middleX - e.clientX) / 10}px)`;
         parallax1.style.backgroundPositionY = `calc(50% + ${(middleY - e.clientY) / 10}px)`;
 
-        parallax2.style.backgroundPositionX = `calc(50% + ${(middleX - e.clientX) / 20}px)`;
-        parallax2.style.backgroundPositionY = `calc(50% + ${(middleY - e.clientY) / 20}px)`;
+
+        parallax2.style.backgroundPositionX = `calc(50% + ${(middleX - e.clientX) / 25}px)`;
+        parallax2.style.backgroundPositionY = `calc(50% + ${(middleY - e.clientY) / 25}px)`;
     }
 
     document.addEventListener('resize', resizeEvent);
